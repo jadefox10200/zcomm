653d5a4b8e4e1222ff80928b85cf1fca7aed8528
jadefox10200 <jadefox10200@gmail.com>
2025-12-19 14:04:12 -0800

    correcting landing for 301 errors

--- frontend/src/landing.tsx
+++ frontend/src/landing.tsx
@@ -1,14 +1,1 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "semantic-ui-css/semantic.min.css";
-import "./index.css";
-import LandingPage from "./LandingPage";
-
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <LandingPage />
-  </React.StrictMode>
-);
+// Removed: This file is not needed. Use index.tsx and LandingPage.tsx for the landing logic.
